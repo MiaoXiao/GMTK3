21b1bcb4008fa47ddffe0944b807ccd107a6b4c4
ricafeng <ricafeng1994@gmail.com>
2019-08-03 15:13:33 -0700

    prefab for resetting player to last checkpoint

---
+++ GMTK3/Assets/Scripts/ResetPlayerTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ResetPlayerTrigger : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            GameManager.Instance.TeleportToLastCheckpoint();
+        }
+    }
+}
