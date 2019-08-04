8ab44ef72aa0e9444cbdea225f4e60c028794026
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-08-03 17:03:33 -0700

    finished selectable block prefab and scripts

--- GMTK3/Assets/Scripts/SelectableBlock.cs
+++ GMTK3/Assets/Scripts/SelectableBlock.cs
@@ -10,19 +10,24 @@ public class SelectableBlock : MonoBehaviour
     [SerializeField]
     private Sprite HoverEmpty;
     [SerializeField]
-    private Sprite ClickEmpty;
+    private Sprite Clicked;
     [SerializeField]
-    private Sprite HoverClick;
+    private Sprite HoverClicked;
     [SerializeField]
-    private Sprite ClickClick;
+    private Sprite Empty;
+
+    private bool IsClicked;
+
+    SpriteRenderer spriteRen;
 
     void Awake(){
-
+        IsClicked = false;
+        spriteRen = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update(){
-
+        
     }
 
     void OnMouseDown(){
@@ -30,8 +35,60 @@ public class SelectableBlock : MonoBehaviour
             return;
         }
         else{
-            GameObject.Destroy(this.gameObject);
+            if (spriteRen.sprite == HoverEmpty)
+            {
+                Debug.Log("Clicked: Change to clicked");
+                spriteRen.sprite = Clicked;
+            }
+            else if (spriteRen.sprite = HoverClicked){
+                Debug.Log("Clicked: Change to empty");
+                spriteRen.sprite = Empty;
+            }
+            else{
+                return;
+            }
         }
     }
 
+    void OnMouseEnter(){
+        if (IsStatic == true){
+            return;
+        }
+        else{
+            if (spriteRen.sprite == Clicked)
+            {
+                Debug.Log("Hover: Change to click consequences");
+                spriteRen.sprite = HoverClicked;
+            }
+            else if (spriteRen.sprite == Empty)
+            {
+                Debug.Log("Hover: Change to before click");
+                spriteRen.sprite = HoverEmpty;
+            }
+            else{
+                return;
+            }
+        }
+    }
+
+    void OnMouseExit(){
+        if (IsStatic == true){
+            return;
+        }
+        else{
+            if (spriteRen.sprite == HoverClicked)
+            {
+                Debug.Log("Hover: Change it back to click");
+                spriteRen.sprite = Clicked;
+            }
+            else if (spriteRen.sprite == HoverEmpty)
+            {
+                Debug.Log("Hover: Change it back to empty");
+                spriteRen.sprite = Empty;
+            }
+            else{
+                return;
+            }
+        }
+    }
 }
