58b9d80175c1979018cc117ca6a829178c119752
ricafeng <ricafeng1994@gmail.com>
2019-08-03 19:19:40 -0700

    key and door prefabs with script implementations player jump height now high enough to jump over blocks

---
+++ GMTK3/Assets/Scripts/DoorBlock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DoorBlock : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && GameManager.Instance.numberOfKeys > 0)
+        {
+            GameManager.Instance.numberOfKeys--;
+            gameObject.SetActive(false);
+        }
+    }
+}
