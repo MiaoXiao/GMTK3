6acd7cc2e9988aa3a15eebbeb0ff67fd1b0530c7
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-08-03 20:39:50 -0700

    updated creature object script

--- GMTK3/Assets/Scripts/CreatureObject.cs
+++ GMTK3/Assets/Scripts/CreatureObject.cs
@@ -4,26 +4,15 @@ public class CreatureObject : MonoBehaviour
 {
     GameObject player;
 
-    private bool InCreature;
-
     void Awake(){
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void Update(){
-        if (Input.GetKeyDown("e") && InCreature)
-        {
-            player.transform.position = new Vector3(0,0,0);
+    void OnTriggerEnter2D(Collider2D other){
+        if (other.tag == "Player") {
             GameManager.Instance.CollectCreature(this);
             gameObject.SetActive(false);
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other){
-        InCreature = true;
-    }
-
-    void OnTriggerExit2D(Collider2D other){
-        InCreature = false;
-    }
 }
