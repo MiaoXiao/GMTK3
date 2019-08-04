90e764d02eac7f6e3443c21305145bcc711a8318
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-08-03 19:59:08 -0700

    added walking and idle sprite animations

--- GMTK3/Assets/Scripts/PlayerController.cs
+++ GMTK3/Assets/Scripts/PlayerController.cs
@@ -11,25 +11,31 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private float distToGround;
     Rigidbody2D rb;
+    Animator anim;
     
     void Awake(){
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update(){
+        anim.SetInteger("State", 0);
         if (Input.GetKey("a")){
             gameObject.transform.position = new Vector3(gameObject.transform.position.x - Time.deltaTime * speed, gameObject.transform.position.y, gameObject.transform.position.z);
+            anim.SetInteger("State", 1);
         }
         if (Input.GetKey("d")){
             gameObject.transform.position = new Vector3(gameObject.transform.position.x + Time.deltaTime * speed, gameObject.transform.position.y, gameObject.transform.position.z);
+            anim.SetInteger("State", 1);
         }
         if (Input.GetKeyDown("w") && IsGrounded()){
+            anim.SetInteger("State", 2);
             rb.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
         }
     }
 
     private bool IsGrounded(){
-        if(rb.velocity.y < 0.1){
+        if (rb.velocity.y < 0.1){
             if (rb.velocity.y > 0.1 * -1){
                 return true;
             }
