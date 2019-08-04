7d1cae29fb72890b331f4b7b8c5df3f037dfd10e
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-08-03 18:51:50 -0700

    adding creature and unselectable platform prefabs and creature functionality

--- GMTK3/Assets/Scripts/CreatureObject.cs
+++ GMTK3/Assets/Scripts/CreatureObject.cs
@@ -3,20 +3,27 @@
 public class CreatureObject : MonoBehaviour
 {
     GameObject player;
+
+    private bool InCreature;
+
     void Awake(){
         player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    void OnTriggerEnter2D(Collider2D other){
-        Debug.Log("Entered trigger");
-        if (Input.GetKeyDown("e") /*&& (other.gameObject.tag == "Player")*/ ){
+    void Update(){
+        if (Input.GetKeyDown("e") && InCreature)
+        {
             Debug.Log("Pressed e");
-            player.transform.position.Set(0, 0, 0);
+            player.transform.position = new Vector3(0,0,0);
         }
     }
 
+    void OnTriggerEnter2D(Collider2D other){
+        InCreature = true;
+    }
+
     void OnTriggerExit2D(Collider2D other){
-        Debug.Log("Exited");
+        InCreature = false;
     }
 
     public void OnMouseDown()
