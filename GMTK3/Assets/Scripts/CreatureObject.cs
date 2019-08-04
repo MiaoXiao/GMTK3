4624804fb8faf8b3df31f51aea8ac435ebc93338
DESKTOP-O0K5VUL\Darron <darrenhoun@gmail.com>
2019-08-03 17:20:01 -0700

    froze z rotation player prefab

--- GMTK3/Assets/Scripts/CreatureObject.cs
+++ GMTK3/Assets/Scripts/CreatureObject.cs
@@ -2,6 +2,10 @@
 
 public class CreatureObject : MonoBehaviour
 {
+    GameObject player;
+    void Awake(){
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
     public void OnMouseDown()
     {
         GameManager.Instance.CollectCreature(this);
